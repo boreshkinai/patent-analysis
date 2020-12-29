be8b7dd7156514ae0d50e23509e5aca9834159a8
Boris Oreshkin <boris.oreshkin@gmail.com>
2020-12-29 17:15:25 -0500

    Dataset type conversion and cleaning in dask

--- data/uspto_bulk_download.py
+++ data/uspto_bulk_download.py
@@ -1,10 +1,12 @@
 from keras.utils import get_file
-from typing import List
+from typing import List, Dict
 import pathlib
 import os
 import pandas as pd
+import numpy as np
 import argparse
 import sys
+import csv
 
 import dask.dataframe as dask
 from dask.distributed import Client
@@ -20,15 +22,29 @@ DATASETS_EXCLUDE_COLUMNS = {'patent.tsv.zip': ['abstract', 'title'],
                             'uspatentcitation.tsv.zip': [],
                             }
 
+numeric = lambda x: pd.to_numeric(x, errors='coerce')
+
 DATASETS_SPEC = {
     'patent': {
-        'drop_columns': ['abstract', 'title'],
         'index': ['id'],
+        'dtype': {'date': 'datetime64[ns]', 'num_claims': 'int16'},
+        'usecols': ['id', 'type', 'number', 'country', 'date', 'title', 'kind', 'num_claims'],
+        'parse_dates': ['date'],
+        'parse_numeric': {'num_claims': np.int16},
+        'drop_columns': [],
     },
     'uspatentcitation': {
-        'drop_columns': [],
         'index': ['patent_id'],
+        'dtype': {},
+        'usecols': [],
+        'parse_dates': [],
+        'parse_numeric': {},
+        'drop_columns': [],
+        
     },
+#     'location': {
+#         'index': ['id'],
+#     },
 }
 
 
@@ -50,19 +66,19 @@ def download_file_cached(file_name: str, drop_columns: List[str], local_storage:
 
 def prepare_datasets_cached(dataset_name: str, 
                             local_storage: str, permanent_storage: str,
-                            drop_columns: List[str], index: List[str]) -> None:
+                            params: Dict) -> None:
     
     pathlib.Path(local_storage).mkdir(parents=True, exist_ok=True)
     pathlib.Path("/tmp/patent-analysis/").mkdir(parents=True, exist_ok=True)
     file_name_zip = f"{dataset_name}.tsv.zip"
     file_name_tsv = f"{dataset_name}.tsv"
-    if True: # not os.path.exists(f"{permanent_storage}/{file_name_zip}"):
-        print(f"Downloading {file_name_zip} locally")
+    if not os.path.exists(os.path.join(permanent_storage, file_name_zip)):
+        print(f"Downloading {file_name_zip} locally in {local_storage}")
         get_file(file_name_zip, origin=os.path.join(ROOT_URL, file_name_zip), 
                  cache_dir=local_storage, cache_subdir='')
         
-        print(f"Unzipping {file_name_zip} locally")
-        cmd = f"unzip -o {local_storage}{file_name_zip} -d {local_storage}"
+        print(f"Unzipping {file_name_zip} locally in {local_storage}")
+        cmd = f"unzip -u {os.path.join(local_storage, file_name_zip)} -d {local_storage}/"
         os.system(cmd)
         
         print("Create dask local cluster")
@@ -71,13 +87,29 @@ def prepare_datasets_cached(dataset_name: str,
     
         print(f"Creating parquete {dataset_name}.parquet")
         ddf = dask.read_csv(os.path.join(local_storage, file_name_tsv),
-                            sep='\t', error_bad_lines=False, dtype=str, engine='python')
-        ddf = ddf.set_index(index)
-        ddf = ddf.drop(columns=drop_columns)
+                            sep='\t', error_bad_lines=False, 
+                            dtype=str, 
+                            usecols=params['usecols'],
+                            quoting=csv.QUOTE_NONNUMERIC,
+#                             engine='python'
+                            )
+        
+        for k in params['parse_dates']:
+            ddf[k] = dask.to_datetime(ddf[k], errors='coerce')
+        ddf = ddf.dropna(subset=params['parse_dates'])
+            
+        for k, v in params['parse_numeric'].items():
+            ddf[k] = dask.to_numeric(ddf[k], errors='coerce')
+            ddf = ddf.dropna(subset=[k])
+            ddf[k] = ddf[k].astype(v)
+            
+        ddf = ddf.set_index(params['index'])
+        ddf = ddf.drop(columns=params['drop_columns'])
+            
         ddf.to_parquet(os.path.join(local_storage, f"{dataset_name}.parquet"), engine='pyarrow', schema="infer")
 
         print(f"Transferring to permanent storage {dataset_name}")
-        os.system(f"rsync -avr --progress {local_storage}* {permanent_storage}/")
+        os.system(f"rsync -hvrt --progress {local_storage}/* {permanent_storage}/")
         
         client.close()
         
@@ -101,11 +133,6 @@ if __name__ == "__main__":
         prepare_datasets_cached(dataset_name=d, 
                                 local_storage=args.local_storage, 
                                 permanent_storage=args.permanent_storage,
-                                drop_columns=params['drop_columns'], 
-                                index=params['index'])
+                                params=params)
         
-#     for d in DATASETS:
-#         download_file_cached(file_name=d,
-#                              drop_columns=DATASETS_EXCLUDE_COLUMNS[d],
-#                              permanent_storage=args.permanent_storage,
-#                              local_storage=args.local_storage)
+    os.system(f"rsync -hvrt --progress {args.permanent_storage}/*parquet {args.local_storage}/")
