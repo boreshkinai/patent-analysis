fd88a8fc4c07cf1c12414fde73635a52477ba216
Boris Oreshkin <boris.oreshkin@gmail.com>
2020-12-28 20:44:38 -0500

    Added dask support in datasets preparation

--- data/uspto_bulk_download.py
+++ data/uspto_bulk_download.py
@@ -1,18 +1,35 @@
-from tensorflow.keras.utils import get_file
+from keras.utils import get_file
 from typing import List
 import pathlib
 import os
 import pandas as pd
 import argparse
+import sys
+
+import dask.dataframe as dask
+from dask.distributed import Client
 
 
 DATA_TIMESTAMP = '20171226'
 ROOT_URL = 'http://www.patentsview.org/data/' + DATA_TIMESTAMP + '/'
 
-DATASETS = ['patent.tsv.zip',
+DATASETS = ['patent.tsv.zip', 
+            'uspatentcitation.tsv.zip',
             ]
 DATASETS_EXCLUDE_COLUMNS = {'patent.tsv.zip': ['abstract', 'title'],
+                            'uspatentcitation.tsv.zip': [],
                             }
+
+DATASETS_SPEC = {
+    'patent': {
+        'drop_columns': ['abstract', 'title'],
+        'index': ['id'],
+    },
+    'uspatentcitation': {
+        'drop_columns': [],
+        'index': ['patent_id'],
+    },
+}
 
 
 def download_file_cached(file_name: str, drop_columns: List[str], local_storage: str, permanent_storage: str) -> None:
@@ -29,6 +46,42 @@ def download_file_cached(file_name: str, drop_columns: List[str], local_storage:
 
         print(f"Transferring to permanent storage {file_name}")
         os.system(f"rsync -avr --progress {local_storage}* {permanent_storage}/")
+        
+
+def prepare_datasets_cached(dataset_name: str, 
+                            local_storage: str, permanent_storage: str,
+                            drop_columns: List[str], index: List[str]) -> None:
+    
+    pathlib.Path(local_storage).mkdir(parents=True, exist_ok=True)
+    pathlib.Path("/tmp/patent-analysis/").mkdir(parents=True, exist_ok=True)
+    file_name_zip = f"{dataset_name}.tsv.zip"
+    file_name_tsv = f"{dataset_name}.tsv"
+    if True: # not os.path.exists(f"{permanent_storage}/{file_name_zip}"):
+        print(f"Downloading {file_name_zip} locally")
+        get_file(file_name_zip, origin=os.path.join(ROOT_URL, file_name_zip), 
+                 cache_dir=local_storage, cache_subdir='')
+        
+        print(f"Unzipping {file_name_zip} locally")
+        cmd = f"unzip -o {local_storage}{file_name_zip} -d {local_storage}"
+        os.system(cmd)
+        
+        print("Create dask local cluster")
+        ncpus = len(os.sched_getaffinity(0))
+        client = Client(n_workers=ncpus, threads_per_worker=1, memory_limit='6GB', dashboard_address='6006')
+    
+        print(f"Creating parquete {dataset_name}.parquet")
+        ddf = dask.read_csv(os.path.join(local_storage, file_name_tsv),
+                            sep='\t', error_bad_lines=False, dtype=str, engine='python')
+        ddf = ddf.set_index(index)
+        ddf = ddf.drop(columns=drop_columns)
+        ddf.to_parquet(os.path.join(local_storage, f"{dataset_name}.parquet"), engine='pyarrow', schema="infer")
+
+        print(f"Transferring to permanent storage {dataset_name}")
+        os.system(f"rsync -avr --progress {local_storage}* {permanent_storage}/")
+        
+        client.close()
+        
+        
 
 
 
@@ -44,8 +97,15 @@ if __name__ == "__main__":
         help='Path to the raw data, unzipped data stored locally in the job storage')
 
     args = parser.parse_args()
-    for d in DATASETS:
-        download_file_cached(file_name=d,
-                             drop_columns=DATASETS_EXCLUDE_COLUMNS[d],
-                             permanent_storage=args.permanent_storage,
-                             local_storage=args.local_storage)
+    for d, params in DATASETS_SPEC.items():
+        prepare_datasets_cached(dataset_name=d, 
+                                local_storage=args.local_storage, 
+                                permanent_storage=args.permanent_storage,
+                                drop_columns=params['drop_columns'], 
+                                index=params['index'])
+        
+#     for d in DATASETS:
+#         download_file_cached(file_name=d,
+#                              drop_columns=DATASETS_EXCLUDE_COLUMNS[d],
+#                              permanent_storage=args.permanent_storage,
+#                              local_storage=args.local_storage)
