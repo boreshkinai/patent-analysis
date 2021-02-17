76c36b336962e6358715a8657758e55b19fedb47
Boris Oreshkin <boris.oreshkin@gmail.com>
2021-02-17 13:46:01 -0500

    Cleaned up the fields downloaded in the API version of the dataset and wrote code to flatten json entires in the API dataset

--- data/load_data_api.py
+++ data/load_data_api.py
@@ -6,17 +6,28 @@ import pathlib
 import gzip
 import os
 import pandas as pd
+from tqdm.auto import tqdm
+from typing import List
 
 START_YEAR = 1976
 START_MONTH = 1
-END_YEAR = 2018
+END_YEAR = 2020
 END_MONTH = 12
 
 NUM_PATENT_PER_PAGE = 100
 
 BASE_URL = 'http://www.patentsview.org/api/patents/query?'
+
 Q_PART = 'q={"_and":[{"_gte":{"patent_date":"2016-01-01"}},{"_lte":{"patent_date":"2016-02-01"}}]}'
-F_PART = '&f=["patent_number","patent_title","patent_abstract","patent_date","patent_year","patent_kind","patent_type","patent_average_processing_time","patent_processing_time","app_number","app_date","app_type","govint_org_id","govint_org_name","govint_org_level_one","govint_org_level_two","govint_org_level_three","govint_contract_award_number","govint_raw_statement","inventor_id","inventor_first_name","inventor_last_name","rawinventor_first_name","rawinventor_last_name","inventor_first_seen_date","inventor_last_seen_date","inventor_location_id","inventor_city","inventor_state","inventor_country","inventor_latitude","inventor_longitude","inventor_lastknown_location_id","inventor_lastknown_city","inventor_lastknown_state","inventor_lastknown_country","inventor_lastknown_latitude","inventor_lastknown_longitude","inventor_sequence","assignee_city","assignee_country","assignee_first_name","assignee_first_seen_date","assignee_id","assignee_lastknown_city","assignee_lastknown_country","assignee_lastknown_latitude","assignee_lastknown_location_id","assignee_lastknown_longitude","assignee_lastknown_state","assignee_last_name","assignee_last_seen_date","assignee_latitude","assignee_location_id","assignee_longitude","assignee_organization","assignee_sequence","assignee_state","assignee_type","appcit_category","appcit_date","appcit_kind","appcit_app_number","appcit_sequence","cited_patent_number","cited_patent_title","cited_patent_date","cited_patent_kind","cited_patent_category","cited_patent_sequence","citedby_patent_number","citedby_patent_title","citedby_patent_date","citedby_patent_kind","citedby_patent_category","cpc_section_id","cpc_subsection_id","cpc_group_id","cpc_subgroup_id","cpc_category","cpc_subsection_title","cpc_group_title","cpc_subgroup_title","cpc_first_seen_date","cpc_last_seen_date","cpc_sequence","ipc_section","ipc_class","ipc_subclass","ipc_main_group","ipc_subgroup","ipc_first_seen_date","ipc_last_seen_date","ipc_sequence","ipc_symbol_position","ipc_action_date","ipc_classification_data_source","ipc_classification_value","ipc_version_indicator","nber_category_id","nber_subcategory_id","nber_category_title","nber_subcategory_title","nber_first_seen_date","nber_last_seen_date","uspc_mainclass_id","uspc_subclass_id","uspc_mainclass_title","uspc_subclass_title","uspc_first_seen_date","uspc_last_seen_date","uspc_sequence","wipo_field_id","wipo_sector_title","wipo_field_title","wipo_sequence"]'
+
+F_PART = '&f=["patent_number","patent_title","patent_date","patent_kind","patent_type",\
+"app_number","app_date","app_type",\
+"inventor_id","inventor_first_name","inventor_last_name","inventor_location_id","inventor_city","inventor_state","inventor_country","inventor_latitude","inventor_longitude","inventor_sequence",\
+"assignee_id","assignee_organization","assignee_first_name","assignee_last_name","assignee_location_id","assignee_city","assignee_state","assignee_country","assignee_latitude","assignee_longitude","assignee_sequence","assignee_type",\
+"cited_patent_number","cited_patent_date","cited_patent_sequence",\
+"citedby_patent_number","citedby_patent_date",\
+"cpc_section_id","cpc_subsection_id","cpc_group_id","cpc_subgroup_id","cpc_category","cpc_sequence"]'
+
 # SORT_PART = '&s=[{"patent_title":"asc"}]'
 DEFAULT_FORMAT_PART = '&o={"page":1,"per_page":%d}' %(NUM_PATENT_PER_PAGE)
 
@@ -82,11 +93,16 @@ def fetch_month_data(year, month):
     return patents, is_success, log_txt
 
 
-def get_month_file_name(logdir, year, month):
+def get_month_file_name(logdir, year, month: str) -> str:
     return logdir + '%d-%d' %(year,month) + '.json'
 
-def get_month_file_zipped_name(json_file_name):
+
+def get_month_file_zipped_name(json_file_name: str) -> str:
     return json_file_name + '.gz'
+
+
+def get_month_file_parquet_name(json_file_name: str) -> str:
+    return ".".join(json_file_name.split(".")[:-1]) + '.parquet'
 
 
 def save_month_data(json_file_name, patents, is_success, log_txt):
@@ -95,27 +111,53 @@ def save_month_data(json_file_name, patents, is_success, log_txt):
     if is_success:
         with open(json_file_name, 'w') as fp:
             json.dump(patents, fp)
-        with open(json_file_name, 'rb') as fp, gzip.open(gz_file, 'wb') as fzip:
-            fzip.writelines(fp)
-        os.remove(json_file_name)
 
         if os.path.isfile(log_file_name):
             os.remove(log_file_name)
     else:
         with open(log_file_name, 'w') as fp:
             fp.write(log_txt)
+            
+            
+def flatten_column(df: pd.DataFrame, column: str, index: str) -> pd.DataFrame:
+    flat_list = []
+    for row in tqdm(df.iterrows(), total=len(df)):
+        if isinstance(row[1][column], list):
+            x = pd.json_normalize(row[1][column])
+            x.index = [row[1][index] for i in range(len(x))]
+            flat_list.append(x)
+        
+    return pd.concat(flat_list)
+
+
+def flatten_dataframe(df: pd.DataFrame, columns:List[str], index: str) -> pd.DataFrame:
+    df_out = df.drop(columns=columns)
+    df_out = df_out.set_index(index)
+    for c in columns:
+        df_c = flatten_column(df, column=c, index=index)
+        df_out = df_out.join(df_c)
+    return df_out
 
 
 pathlib.Path(RESULTS_DIR).mkdir(parents=True, exist_ok=True)
 for year in range(START_YEAR, END_YEAR+1):
     for month in range(START_MONTH, END_MONTH + 1):
         print('Processing year %d month %d' %(year, month))
-
+        
         json_file_name = get_month_file_name(RESULTS_DIR, year, month)
-
-        if not os.path.isfile( get_month_file_zipped_name(json_file_name) ):
+        if not os.path.isfile(json_file_name):
             patents, is_success, log_txt = fetch_month_data(year, month)
-
             save_month_data(json_file_name, patents, is_success, log_txt)
+            
+        parquet_file_name = get_month_file_parquet_name(json_file_name)
+        if not os.path.isfile(parquet_file_name):
+            df = pd.read_json(json_file_name)
+            if not df.empty:
+                df_flat = flatten_dataframe(df, index="patent_number",
+                                            columns=["inventors", "assignees", "cited_patents", 
+                                                     "citedby_patents", "cpcs"])
+                df_flat.to_parquet(parquet_file_name)
+            
+        
 
 
